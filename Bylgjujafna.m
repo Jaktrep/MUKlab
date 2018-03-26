50616c60ee873d4b00eab5df49cd9eaa87676ae7
Gaasi <vltrkri8@gmail.com>
2018-03-26 16:41:31 +0000

    Setja inn það sem Matti gerði

--- Bylgjujafna.m
+++ Bylgjujafna.m
@@ -1,8 +1,12 @@
 function B = Bylgjujafna(L,T,N,M,gamma)
+%Skiptipunktar skilgreindir:
 h = L/N;
 tau = T/M;
 xj = 0:h:L;
 tk = 0:tau:T;
+%Fylki skilgreint:
+Q = (N+1)*(M+1);
+A = sparse(Q,Q);
 
 
 end
