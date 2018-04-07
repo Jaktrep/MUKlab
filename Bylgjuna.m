421c223144c47f46fe7da00a106b05abf0620005
mrharksen <matthias.harksen@gmail.com>
2018-04-07 21:25:54 +0000

    Update Bylgjuna.m

--- Bylgjuna.m
+++ Bylgjuna.m
@@ -15,8 +15,12 @@ function B = Bylgjujafna(L,T,N,M,gamma)
   end
   for j=1:N-1
      i =  (N+1) + j+1;
-     A(i,i-(N+1):N+1:i+(N+1)) = A(i,i-(N+1):N+1:i+(N+1)) + [-gamma^2*h^(-2) (2*tau^(-2)+2*gamma^2*h^(-2)) -gamma^2*h^(-2)];
+     A(i,i-1:i+1) = A(i,i-1:i+1) + [-gamma^2*h^(-2) (2*tau^(-2)+2*gamma^2*h^(-2)) -gamma^2*h^(-2)];
   end
+  bvec = f0*ones(Q,1);
+  bvec(N+1:N+1:Q) = 2*sin(6*pi.*tk/T);
+  bvec(1:N+1) = 0;
+  bvec(N+2:N+1:Q) = 0;
   for j=0:N
       i = j+1;
       A(i,:) = [zeros(1,i-1) 1 zeros(1,Q-i)];
@@ -29,14 +33,10 @@ function B = Bylgjujafna(L,T,N,M,gamma)
      i = (N+1)*(k+1);
      A(i,:) = [zeros(1,i-1) 1 zeros(1,Q-i)];
   end
-  bvec = f0*ones(Q,1);
-  bvec(1:N+1) = 0;
-  bvec(N+2:N+1:Q) = 0;
-  bvec(N+1:N+1:Q) = 2*sin(6*pi.*tk/T);
   cvec = A\bvec;
   B = zeros(M+1,N+1);
   for k = 0:M
     B(k+1,:) = cvec(k*(N+1) + 1:(k+1)*(N+1));
   end
-  B = transpose(B);
+  B = sparse(transpose(B));
 end
