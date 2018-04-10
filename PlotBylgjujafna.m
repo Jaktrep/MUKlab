5df4964973fe4459ad0544a863ad327446a0b479
mrharksen <matthias.harksen@gmail.com>
2018-04-10 19:36:22 +0000

    Plotplot

--- PlotBylgjujafna.m
+++ PlotBylgjujafna.m
@@ -2,12 +2,41 @@ L = 1;
 T = 10;
 N = 100;
 M = 100;
+h = L/N;
+tau = T/M;
 gamma = 0.5;
-C = Bylgjujafna(L,T,N,M,gamma);
-xj = 0:(L/N):L; 
-plot(xj,C(21,:))
-hold on
-K = importdata('/Users/mbh/Desktop/bgildi.mat');
-isequal(K,C)
-plot(xj,K(21,:))
-hold off
+xj = 0:h:L;
+tk = 0:tau:T;
+
+
+B = Bylgjujafna(L,T,N,M,gamma);
+
+figure(1)
+plot(xj,B(:,21));   
+xlabel('x');
+ylabel('u(x,t)');
+title('Bylgjujafnan: nálgunarlausn u(x,t) við tímann t = 2');
+
+figure(2)
+plot(xj,B(:,41));    
+xlabel('x');
+ylabel('u(x,t)');
+title('Bylgjujafnan: nálgunarlausn u(x,t) við tímann t = 4');
+
+figure(3)
+plot(xj,B(:,61));  
+xlabel('x');
+ylabel('u(x,t)');
+title('Bylgjujafnan: nálgunarlausn u(x,t) við tímann t = 6');
+
+figure(4)
+plot(xj,B(:,81));   
+xlabel('x');
+ylabel('u(x,t)');
+title('Bylgjujafnan: nálgunarlausn u(x,t) við tímann t = 8');
+
+figure(5)
+plot(xj,B(:,101));   
+xlabel('x');
+ylabel('u(x,t)');
+title('Bylgjujafnan: nálgunarlausn u(x,t) við tímann t = 10');
