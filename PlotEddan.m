0a9574bf0f12698dcbe1e530cdf6cc9e25dac23c
mrharksen <matthias.harksen@gmail.com>
2018-04-12 17:42:00 +0000

    Update PlotEddan.m

--- PlotEddan.m
+++ PlotEddan.m
@@ -15,7 +15,9 @@ zlabel('u(x,y)');
 title('Þrívítt graf af fletinum');
 
 figure(2)
-contour(xi,yj,V)    
+vaporwave;
+colormap(C);
+contour(xi,yj,V,100)   
 xlabel('x');
 ylabel('y');
 title('Jafnhæðarlínur flatarins');
